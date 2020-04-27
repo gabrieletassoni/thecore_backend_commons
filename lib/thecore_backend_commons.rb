7472428705bfefe51db1ebb4487a3ce3b8d3f689
Gabriele Tassoni <gabriele.tassoni@gmail.com>
2020-04-27 15:23:25 +0200

    Add http_accept_language

--- lib/thecore_backend_commons.rb
+++ lib/thecore_backend_commons.rb
@@ -2,6 +2,7 @@ require "thecore_backend_commons/engine"
 
 require 'thecore_auth_commons'
 require 'devise-i18n'
+require 'http_accept_language'
 
 module ThecoreBackendCommons
   # Your code goes here...
