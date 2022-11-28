45736ac454d680cfc7747dc8d43b19b9a406a5ff
Gabriele Tassoni <g.tassoni@bancolini.com>
2022-11-28 17:00:36 +0000

    Add , if_not_exists: true to migrations

--- db/migrate/20220117135551_create_action_mailbox_tables.action_mailbox.rb
+++ db/migrate/20220117135551_create_action_mailbox_tables.action_mailbox.rb
@@ -1,7 +1,7 @@
 # This migration comes from action_mailbox (originally 20180917164000)
 class CreateActionMailboxTables < ActiveRecord::Migration[6.0]
   def change
-    create_table :action_mailbox_inbound_emails do |t|
+    create_table :action_mailbox_inbound_emails, if_not_exists: true do |t|
       t.integer :status, default: 0, null: false
       t.string  :message_id, null: false
       t.string  :message_checksum, null: false
