908899260677bdaf8fbc09b8cada3bd5c652efac
Gabriele Tassoni <g.tassoni@bancolini.com>
2023-04-04 16:47:13 +0000

    Add Action Cable support out of the box

--- app/channels/activity_log_channel.rb
+++ app/channels/activity_log_channel.rb
@@ -1,10 +1,16 @@
 class ActivityLogChannel < ApplicationCable::Channel
   def subscribed
-    # stream_from "some_channel"
-    stream_from "activity_log"
+    stream_from "messages"
   end
 
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
+  end
+
+  # https://cableready.stimulusreflex.com/troubleshooting/#verify-actioncable
+
+  def receive(data)
+    puts data["message"]
+    ActionCable.server.broadcast("messages", "ActionCable is connected")
   end
 end
