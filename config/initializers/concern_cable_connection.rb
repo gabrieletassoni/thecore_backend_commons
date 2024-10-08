7df7bb8a46b9314262fa8b1c8e8b7dacbc0d66cc
Gabriele Tassoni <g.tassoni@bancolini.com>
2024-10-08 14:21:50 +0000

    Add Check for token both from JWT or from encrypted_access_token

--- config/initializers/concern_cable_connection.rb
+++ config/initializers/concern_cable_connection.rb
@@ -16,12 +16,17 @@ module CableConnectionConcern
       # It looks for a token in the query parameters, or in the headers
       m = request.query_parameters["token"].presence || request.headers["Authorization"].split(" ").second.strip rescue nil
 
-      body = ::HashWithIndifferentAccess.new(::JWT.decode(m, ::Rails.application.credentials.dig(:secret_key_base).presence||ENV["SECRET_KEY_BASE"], false)[0]) rescue nil
-      if verified_user = (env['warden'].user.presence || User.find_by(id: body[:user_id]) rescue false)
-        verified_user
-      else
-        reject_unauthorized_connection
-      end
+      # check for m2m token
+      user = User.where.not(encrypted_access_token: nil).find { |u| BCrypt::Password.new(u.encrypted_access_token) == m } rescue false
+      return user if user
+
+      # check for JWT token
+      body = (::HashWithIndifferentAccess.new(::JWT.decode(m, ::Rails.application.credentials.dig(:secret_key_base).presence||ENV["SECRET_KEY_BASE"], false)[0]) rescue nil)
+      verified_user = env['warden'].user.presence || User.find_by(id: body[:user_id]) rescue false
+      return verified_user if verified_user
+
+      # The Token is not m2m nor JWT, fail
+      return reject_unauthorized_connection
     end
   end
 end
--- lib/thecore_backend_commons/version.rb
+++ lib/thecore_backend_commons/version.rb
@@ -1,3 +1,3 @@
 module ThecoreBackendCommons
-  VERSION = "3.2.3"
+  VERSION = "3.2.4"
 end
