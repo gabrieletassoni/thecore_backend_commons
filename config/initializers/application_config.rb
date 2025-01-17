2df6138a9b1cb020123a62c9f073934628fe946c
Gabriele Tassoni <g.tassoni@bancolini.com>
2025-01-17 16:41:02 +0000

    Step by step debug for sub-uri

--- config/initializers/application_config.rb
+++ config/initializers/application_config.rb
@@ -2,5 +2,8 @@ Rails.application.config.relative_url_root = ENV.fetch("RAILS_RELATIVE_URL_ROOT"
 Rails.application.config.assets.prefix = "#{ENV.fetch("RAILS_RELATIVE_URL_ROOT", "")}/assets".gsub('//', '/')
 
 Rails.application.config.active_storage.configure :Disk, root: Rails.root.join("storage")
+Rails.application.config.active_storage.routes_prefix = ENV.fetch("RAILS_RELATIVE_URL_ROOT", "")
+
 Rails.application.config.action_mailer.delivery_method = :smtp
+
 Rails.application.config.action_cable.allowed_request_origins = [/http:\/\/*/, /https:\/\/*/, /file:\/\/*/, 'file://', nil]
